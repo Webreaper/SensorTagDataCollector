cf8fe1f6618dc74f9edf8069ac826acc6008fe53
Mark Otway <mark@otway.com>
2021-04-29 10:09:18 +0100

    Correct logfile name

--- SensorTagDataCollector/Utils/LogHandler.cs
+++ SensorTagDataCollector/Utils/LogHandler.cs
@@ -26,7 +26,7 @@ namespace Logging
             logger = new LoggerConfiguration()
                 .MinimumLevel.ControlledBy(logLevel)
                 .WriteTo.Console(outputTemplate: template)
-                .WriteTo.File("QbtCleanup-.log", outputTemplate: template,
+                .WriteTo.File("SensorTag-.log", outputTemplate: template,
                                rollingInterval: RollingInterval.Day,
                                fileSizeLimitBytes: 104857600)
                 .CreateLogger();
